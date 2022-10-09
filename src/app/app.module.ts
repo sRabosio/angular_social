d8d769a221c9cb276d520cf2cea84168e5183c8b
sRabosio <rabosiosimone@gmail.com>
2022-10-09 13:19:18 +0200

    components defs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PostComponent,
+    CommentComponent,
+    ProfileComponent,
+    SidebarComponent,
+    NewPostComponent,
+    PostsListComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
