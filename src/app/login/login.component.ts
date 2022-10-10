355790f526289739094730e90ce70277c802bb11
sRabosio <rabosiosimone@gmail.com>
2022-10-10 19:21:48 +0200

    login email binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,12 +14,24 @@ export class LoginComponent implements OnInit {
     password: "amogus",
   }
 
-  private formEmail: string = ""
-  private formPassword:string = ""
+  protected formEmail: string = ""
+  protected formPassword:string = ""
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onEvent(e:Event){
+    const input = e.target as HTMLInputElement
+    if(input.name !== "email") return
+    const value = input.value
+    const at = value.indexOf("@")
+    if(at >= 0 && value.indexOf(".") > at) return
+  }
+
+  onLogin(email:HTMLInputElement, password:HTMLInputElement){
+    console.log(`${email.value} ${password.value}`);
   }
 
 }
