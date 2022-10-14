cb87bd10c63d23380e7aa2d8ff88da2ae7853098
sRabosio <rabosiosimone@gmail.com>
2022-10-14 22:33:27 +0200

    basic router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/data-types/user';
+import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_social';
+  user?:User
+
+  fetchUser = new Promise(()=>{
+    let sessionUser: User | undefined
+    const i = setInterval(()=>{
+      sessionUser = this.session.user
+      if(sessionUser){
+        this.user = sessionUser
+        clearInterval(i)
+      } 
+    },1000)
+  })
+
+  constructor(private session:SessionService){
+
+  }
 }
