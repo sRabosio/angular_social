cd6c3fd576554aa349e5effd9ca0cd6770b4444a
sRabosio <rabosiosimone@gmail.com>
2022-10-19 17:23:19 +0200

    newpost invalid messages

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -15,6 +15,22 @@ export class NewPostComponent implements OnInit, DoCheck {
   form:FormGroup
   protected postTitle:string = ""
 
+  get titleInvalidMessage(){
+    const title = this.form.get('postTitle')
+    if(!title) return null
+
+    if(title.value.length > 50) return "title too long"
+    return null
+  }
+
+  get contentInvalidMessage(){
+    const content = this.form.get('postContent')
+    if(!content) return null
+
+    if(content.value.length > 300) return "text too long"
+    return null
+  }
+
   constructor(private userService:UserService, private postService:PostService, private session:SessionService) {
     this.form = new FormGroup({
       'postTitle': new FormControl('',[Validators.required, Validators.maxLength(50)]),
@@ -29,6 +45,7 @@ export class NewPostComponent implements OnInit, DoCheck {
   }
 
   onPost(){
+    if(!this.form.valid) return
     const user = this.session.user
     if(!user){
       alert("l'utente non esiste!")
